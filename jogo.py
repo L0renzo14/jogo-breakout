d342302906f088ab230d8af2c1dae70132c4de10
Lorenzo Pasqualotto <lorenzo.cc.pp@gmail.com>
2022-11-20 18:53:02 -0300

    ajustes finais

--- jogo.py
+++ jogo.py
@@ -55,19 +55,19 @@ def redefinir():
 	tempo_atual = pygame.time.get_ticks()
 
 	if tempo_atual - tempo_perdido < 1000:
-		num_three = fonte_conometro.render("3", True, cor)
-		num_three_rect = num_three.get_rect(center=(largura//2, altura//2+10))
-		tela.blit(num_three, num_three_rect)
+		numero_tres = fonte_conometro.render("3", True, cor)
+		numero_tres_rect = numero_tres.get_rect(center=(largura//2, altura//2+10))
+		tela.blit(numero_tres, numero_tres_rect)
 
 	elif 1000 < tempo_atual - tempo_perdido < 2000:
-		num_two = fonte_conometro.render("2", True, cor)
-		num_two_rect = num_two.get_rect(center=(largura//2, altura//2+10))
-		tela.blit(num_two, num_two_rect)
+		numero_dois = fonte_conometro.render("2", True, cor)
+		numero_dois_rect = numero_dois.get_rect(center=(largura//2, altura//2+10))
+		tela.blit(numero_dois, numero_dois_rect)
 
 	elif 2000 <tempo_atual - tempo_perdido < 3000:
-		num_one = fonte_conometro.render("1", True, cor)
-		num_one_rect = num_one.get_rect(center=(largura//2, altura//2+10))
-		tela.blit(num_one, num_one_rect)
+		numero_um = fonte_conometro.render("1", True, cor)
+		numero_um_rect = numero_um.get_rect(center=(largura//2, altura//2+10))
+		tela.blit(numero_um, numero_um_rect)
 
 	if tempo_atual - tempo_perdido >= 3000:
 		posicao_x_bola = - posicao_x_bola
@@ -75,17 +75,17 @@ def redefinir():
 		tempo_perdido = None
 
 def escritaPontuacao():
-	pontuacao_text = fonte_pontuacao.render(f"{pontuacao}",True,cor)
-	pontuacao_rect = pontuacao_text.get_rect(center= (20,altura-20))
-	tela.blit(pontuacao_text, pontuacao_rect)
+	pontuacao_texto = fonte_pontuacao.render(f"{pontuacao}",True,cor)
+	pontuacao_rect = pontuacao_texto.get_rect(center= (20,altura-20))
+	tela.blit(pontuacao_texto, pontuacao_rect)
 
 def win():
 	if len(listaDeBlocos) == 0:
 		tela.fill(cor_do_fundo)
 		posicao_y_bola, posicao_x_bola = 0,0
-		won_text = fonte_conometro.render("You won !!",True,cor)
-		won_rect = won_text.get_rect(center = (largura//2, altura//2))
-		tela.blit(won_text, won_rect)
+		texto_de_vitoria = fonte_conometro.render("You won !!",True,cor)
+		vitoria_rect = texto_de_vitoria.get_rect(center = (largura//2, altura//2))
+		tela.blit(texto_de_vitoria, vitoria_rect)
 
 pygame.init()
 largura = 800 #tupla
